e012f3641e22687cdeaf2a215a2facdf436c501b
Allen Zhao <>
2023-06-19 19:59:48 -0700

    initial commit

---
+++ scripts/order_lister.py
@@ -0,0 +1,31 @@
+import csv
+filename = "../data/Sorting Copy of Summer 2023 GrandWall Order Form Spreadsheet - Form responses 1.csv"
+
+with open(filename, "r") as f:
+    orders = list(csv.DictReader(f))
+    del orders[0]
+
+fug = '''What is your VOC Membership ID? To find this you have to login to your VOC account. 
+1. Go to https://www.ubc-voc.com
+2. Go to "Member's Section" at the top of the page
+3. Click on "Login"
+4. Input your "Username" and "Password"
+5. Go to "Member's Section" at the top of the page
+6. Click on "Edit Account"
+7. Find your "Member ID" bullet point (it should be the last bullet point)'''
+
+
+for num, order in enumerate(orders):
+    print("==== {}'s Order (#{})' ====".format(order["Name:"], num))
+    email = order["Email address"]
+    membership_id = order[fug]
+    del order[fug]
+    ordered_items = filter(
+        lambda item: True if item[1] != "" and item[1].isnumeric() else False,
+        order.items())
+    for item, amount in ordered_items:
+        print("{}: (x{})".format(item, amount))
+    print("Email: {}\n"
+          + "VOCID: https://www.ubc-voc.com/members/show_extended?target_id={}"
+          .format(email, membership_id))
+    input("Press Enter to continue...")
